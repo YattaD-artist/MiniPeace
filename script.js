732dc37eb245e820e9a1ab3af7b6fbed47c3fdf2
YattaD <Yattadwork@gmail.com>
2025-08-23 01:12:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -313,3 +313,17 @@ toggleBtn.addEventListener('click', () => {
     img.src = src;
   });
 });
+
+// ====== APP =====
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.log("Service Worker registration failed:", error);
+      });
+  });
+}
